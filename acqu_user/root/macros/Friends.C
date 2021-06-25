efa0139a153544e33990493c4b0019e9eed5d131
David Hornidge <dhornidge@mta.ca>
2021-06-25 12:01:38 -0300

    modified:   root/macros/Friends.C

--- acqu_user/root/macros/Friends.C
+++ acqu_user/root/macros/Friends.C
@@ -1,5 +1,8 @@
 {
 
+	TCanvas *c1 = new TCanvas( "c1", "Truth Variables", 20, 20, 700, 700);
+	c1->Divide( 1, 2);
+
 	TFile f1("a2GoAT/TA2Pi0ComptonMC.root","READ");
 
 //	f1.ls()
@@ -8,6 +11,11 @@
 
 	Pi0ComptonTree->AddFriend("EvGenTree","evgen/pi0_p_chan85_in.root");
 
+	c1->cd( 1);
 	Pi0ComptonTree->Draw("Pi0Theta:EvGenTree.ScatTheta", "", "colz");
+
+	c1->cd( 2);
+
+	Pi0ComptonTree->Draw("Pi0Theta-EvGenTree.ScatTheta");
 
 }
